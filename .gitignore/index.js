a76607f753ead7df46e898a6dd36c60960203e73
Blarx <30467716+DjBlarx@users.noreply.github.com>
2018-10-24 10:59:00 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({autoReconnect: true, max_message_cache: 0});
+const spamCounter = new Set();
+
+client.on('message', function (message) {
+  let msgauthor = message.author
+    if (spamCounter.has(msgauthor)) try {
+        message.delete()
+        message.author.send("Tu dois attendre **1 seconde** avant de pouvoir envoyer un autre message.")
+    }catch (error){} else {
+    spamCounter.add(msgauthor), setTimeout(() => { spamCounter.delete(msgauthor) }, 2000)
+    }
+    if (!message.author.bot) if (message.content.includes("https://discord.gg/") || message.content.includes("https://discordapp.com/invite/")) try {message.author.send("Vous n'avez pas l'autorisation de poster des liens vers d'autres discord."), message.delete()} catch (error) {console.log(error)}
+})
+
+client.on('ready', () => console.log('OK'));
+client.login(config.token)
